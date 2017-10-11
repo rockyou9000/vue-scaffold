1cd1e9d9a01fe32b3d3cd16ad546ecb07d1946f1
yepeng <yepeng@bilibili.com>
2017-10-11 15:32:12 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  'globals': {
+    '_': true,
+    'Vue': true,
+    'jQuery': true,
+    '$': true,
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'import/prefer-default-export': 0,
+    'no-param-reassign': 0,
+    'prefer-const': 0,
+    'no-shadow': 0,
+    'consistent-return': 0,
+  }
+}
